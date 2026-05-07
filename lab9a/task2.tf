faffd7112f562b13a9bdc434cae6332f3c5a0e96
Serhii773 <serhii.feniuk.23@pnu.edu.ua>
2026-05-07 10:04:39 +0300

    Create task2.tf

---
+++ lab9a/task2.tf
@@ -0,0 +1,10 @@
+resource "azurerm_linux_web_app_slot" "staging" {
+  name           = "staging"
+  app_service_id = azurerm_linux_web_app.webapp.id
+
+  site_config {
+    application_stack {
+      php_version = "8.2"
+    }
+  }
+}
