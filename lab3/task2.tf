1f93415b11840235562046b3fe795690c0e1bd7a
Serhii773 <serhii.feniuk.23@pnu.edu.ua>
2026-05-07 03:39:09 +0300

    Complete task2.tf

---
+++ lab3/task2.tf
@@ -0,0 +1,8 @@
+resource "azurerm_managed_disk" "disk2" {
+  name                 = "az104-disk2"
+  location             = azurerm_resource_group.rg3.location
+  resource_group_name  = azurerm_resource_group.rg3.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 32
+}
