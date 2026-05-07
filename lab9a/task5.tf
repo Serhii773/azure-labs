964a4aa6553ad158d5c563de1ea4a63f4559840b
Serhii773 <serhii.feniuk.23@pnu.edu.ua>
2026-05-07 10:06:06 +0300

    Completed task5.tf

---
+++ lab9a/task5.tf
@@ -0,0 +1,56 @@
+resource "azurerm_monitor_autoscale_setting" "webapp_autoscale" {
+  name                = "az104-webapp-autoscale"
+  resource_group_name = azurerm_resource_group.rg9.name
+  location            = azurerm_resource_group.rg9.location
+  target_resource_id  = azurerm_service_plan.app_plan.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 2
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.app_plan.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.app_plan.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 30
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
+}
